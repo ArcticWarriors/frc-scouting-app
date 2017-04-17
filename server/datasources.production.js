4c7518587c66f67bf976e1d5b84fd425886d1080
ProgBot <ProgBot@users.noreply.github.com>
2017-04-17 12:22:15 -0400

    Initial code

---
+++ server/datasources.production.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: {
+    name: 'db',
+    connector: 'mongodb',
+    hostname: process.env.DB_HOST,
+    port: process.env.DB_PORT || 27017,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'frc-scouting',
+  }
+}
