1a2a7a07ae7cbc1e9803e5dfc32b24eae4d50244
wilsonaar <wilsonaar@mylcsd.org>
2017-04-19 14:31:46 -0400

    bootstrapless  " navbar " , removed the vue default template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+
+    <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+              data-target="#navbar" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            <!--<img class="navbar-brand" src="{% static 'BaseScouting/img/174_Favicon.jpg' %}" style="width:50px; padding:0px; margin-right:5px" /> -->
+            </div>
+            <div class="collapse navbar-collapse" id="navbar">
+              <ul class="nav navbar-nav">
+                <li> <a href="">Home</a> </li>
+                <li> <a href="">Team List</a></li>
+                <li> <a href="">Matches</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li> <a href="">Log In</a> </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
     <router-view></router-view>
   </div>
 </template>
@@ -11,13 +37,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+    /* @import 'bootstrap/dist/css/bootstrap.css'; */
 </style>
