ac86cacee60d6eb48ec91833ded9ada7abf1a2c3
Anthony-fay20 <cfay1969@yahoo.com>
2018-02-06 20:16:14 -0500

    more fixes
    more fixes

--- src/components/nav/LoginDialog.vue
+++ src/components/nav/LoginDialog.vue
@@ -7,7 +7,7 @@
     >
       <v-card>
         <v-card-title>Login</v-card-title>
-        <v-alert v-if="error" type="error" :value="true">
+        <v-alert type="error" :value="error">
           {{ error }}
         </v-alert>
         <v-card-text>
@@ -33,7 +33,7 @@
         </v-card-text>
         <v-card-actions>
           <v-btn color="primary" dark @click="login()">Login</v-btn> 
-          <v-btn color="primary" flat @click.stop="$emit=false">Close</v-btn>
+          <v-btn color="primary" flat @click.stop="$emit('input', false)">Close</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
