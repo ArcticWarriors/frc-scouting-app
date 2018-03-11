f6244170f866bf47b2f0ae35968fbc466eccb901
Michael Geary <gearymich@gmail.com>
2018-03-10 22:33:03 -0500

    RankingData pt.2
    Team data table that includes editing, deleting, and a search bar. No
    API or text extension added yet. On new branch from dev, just for John
    <3

--- src/pages/Rankings.vue
+++ src/pages/Rankings.vue
@@ -1,16 +1,186 @@
 <template>
   <div>
-    <p>rankings</p>
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-btn color="primary" dark slot="activator" class="mb-2">New Item</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ formTitle }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Calories" v-model="editedItem.calories"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Boolean" v-model="editedItem.boolean"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Carbs (g)" v-model="editedItem.carbs"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Protein (g)" v-model="editedItem.protein"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+	<v-card-title>
+      Nutrition
+      <v-spacer></v-spacer>
+    <v-text-field
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        v-model="search"
+    ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+    >
+      <template slot="items" slot-scope="props">
+        <td>Team {{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.points }}</td>
+        <td class="text-xs-right">{{ props.item.boolean }}</td>
+        <td class="text-xs-right">{{ props.item.carbs }}</td>
+        <td class="text-xs-right">{{ props.item.protein }}</td>
+        <td class="justify-center layout px-0">
+          <v-btn icon class="mx-0" @click="editItem(props.item)">
+            <v-icon color="teal">edit</v-icon>
+          </v-btn>
+          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+            <v-icon color="pink">delete</v-icon>
+          </v-btn>
+        </td>
+      </template>
+      <v-alert slot="no-results" :value="true" color="error" icon="warning">
+        Your search for "{{ search }}" found no results.
+      </v-alert>
+      <template slot="no-data">
+        <v-btn color="primary" @click="initialize">Reset</v-btn>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Rankings',
-  data() {
-    return {};
-  },
-};
+  export default {
+    data: () => ({
+      search: '',
+      dialog: false,
+      headers: [
+        {
+          text: 'Teams',
+          align: 'center',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Points Per Game', value: 'points' },
+        { text: 'Boolean', value: 'boolean' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Actions', value: 'name', sortable: false }
+      ],
+      items: [],
+      editedIndex: -1,
+      editedItem: {
+
+        name: 0,
+        points: 0,
+        boolean: '',
+        carbs: 0,
+        protein: 0
+      },
+      defaultItem: {
+        name: 0,
+        points: 0,
+        boolean: '',
+        carbs: 0,
+        protein: 0
+      }
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      }
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      }
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.items = [
+          {
+            name: 174,
+            points: 159,
+            boolean: 'true',
+            carbs: 24,
+            protein: 4.0
+          },
+          {
+            name: 3789,
+            points: 237,
+            boolean: 'false',
+            carbs: 37,
+            protein: 4.3
+          },
+          {
+            name: 175,
+            points: 262,
+            boolean: 'true',
+            carbs: 23,
+            protein: 6.9
+          }
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.items.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        const index = this.items.indexOf(item)
+        confirm('Are you sure you want to delete this item?') && this.items.splice(index, 1)
+      },
+
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.items[this.editedIndex], this.editedItem)
+        } else {
+          this.items.push(this.editedItem)
+        }
+        this.close()
+      }
+    }
+  }
 </script>
 
 <style scoped>
