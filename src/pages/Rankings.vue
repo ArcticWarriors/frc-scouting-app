74bb29ba2d59e2a6b172f364a02eed9a8c3e1d40
Michael Geary <gearymich@gmail.com>
2018-03-14 13:38:29 -0400

    More specific sorting, cleaning up

--- src/pages/Rankings.vue
+++ src/pages/Rankings.vue
@@ -1,36 +1,5 @@
 <template>
   <div>
-    <v-dialog v-model="dialog" max-width="500px">
-      <v-btn color="primary" dark slot="activator" class="mb-2">New Item</v-btn>
-      <v-card>
-        <v-card-title>
-          <span class="headline">{{ formTitle }}</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Calories" v-model="editedItem.calories"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Boolean" v-model="editedItem.boolean"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Carbs (g)" v-model="editedItem.carbs"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Protein (g)" v-model="editedItem.protein"></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
 	<v-card-title>
       Nutrition
       <v-spacer></v-spacer>
@@ -48,17 +17,14 @@
       :search="search"
     >
       <template slot="items" slot-scope="props">
-        <td>Team {{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.points }}</td>
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.match }}</td>
         <td class="text-xs-right">{{ props.item.boolean }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
+        <td class="text-xs-right">{{ props.item.points }}</td>
         <td class="text-xs-right">{{ props.item.protein }}</td>
         <td class="justify-center layout px-0">
           <v-btn icon class="mx-0" @click="editItem(props.item)">
             <v-icon color="teal">edit</v-icon>
-          </v-btn>
-          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
-            <v-icon color="pink">delete</v-icon>
           </v-btn>
         </td>
       </template>
@@ -70,10 +36,38 @@
       <v-alert slot="no-results" :value="true" color="error" icon="warning">
         Your search for "{{ search }}" found no results.
       </v-alert>
-      <template slot="no-data">
-        <v-btn color="primary" @click="initialize">Reset</v-btn>
-      </template>
     </v-data-table>
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-btn color="primary" dark slot="activator" class="mb-2">New Item</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ formTitle }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Team" v-model="editedItem.name"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Match #" v-model="editedItem.match"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Boolean" v-model="editedItem.boolean"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Points Exchanged" v-model="editedItem.points"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -83,43 +77,20 @@
       search: '',
       dialog: false,
       headers: [],
-      items: [
-          {
-            name: 174,
-            points: 159,
-            boolean: 'true',
-            carbs: 24,
-            protein: 4.0
-          },
-          {
-            name: 3789,
-            points: 237,
-            boolean: 'false',
-            carbs: 37,
-            protein: 4.3
-          },
-          {
-            name: 175,
-            points: 262,
-            boolean: 'true',
-            carbs: 23,
-            protein: 6.9
-          }
-      ],
+      items: [],
       editedIndex: -1,
       editedItem: {
-
-        name: 0,
+        name: 'Team ' + 0,
+        match: 'Match ' + 0,
         points: 0,
-        boolean: '',
-        carbs: 0,
+        boolean: 0,
         protein: 0
       },
       defaultItem: {
-        name: 0,
-        points: 0,
-        boolean: '',
-        carbs: 0,
+        name: 'Team ' + 0,
+        match: 'Match ' + 0,
+        points: '',
+        boolean: 0,
         protein: 0
       }
     }),
@@ -149,9 +120,9 @@
           sortable: false,
           value: 'name'
         },
-        { text: 'Points Per Game', value: 'points' },
+        { text: 'Match #', value: 'match' },
         { text: 'Boolean', value: 'boolean' },
-        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Points', value: 'points' },
         { text: 'Protein (g)', value: 'protein' },
         { text: 'Actions', value: 'name', sortable: false }
         ]
@@ -161,11 +132,6 @@
         this.editedIndex = this.items.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialog = true
-      },
-
-      deleteItem (item) {
-        const index = this.items.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.items.splice(index, 1)
       },
 
       close () {
